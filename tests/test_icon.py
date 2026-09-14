b64ee66a5887c5d7c56b1c933e0c216dc82d7e37
Wladrian <323729426+wladrian@users.noreply.github.com>
2026-09-14 12:26:20 +0300

    Add simple test case

--- tests/test_icon.py
+++ tests/test_icon.py
@@ -1,7 +1,22 @@
 import pytest
+from pystray import Menu, MenuItem
 from alert_tray_icon.icon import TrayIcon
 
 
 def test_icon_creation():
-    pass
+    def on_exit():
+        pass
 
+    test_menu = Menu(
+        MenuItem("Exit", on_exit),
+    )
+    icon = TrayIcon(
+        title="Test title",
+        color="red",
+        menu=test_menu,
+        notifications=True,
+        app_name="Test app name",
+    )
+
+    assert icon.title == "Test title"
+    assert icon.color == "red"
